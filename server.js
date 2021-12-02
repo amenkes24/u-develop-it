dc455f2fecfc36b24a587b7ccfbab358c921346d
Adam Menkes <amenkes24@gmail.com>
2021-12-01 18:21:06 -0800

    slight update to server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express ();
@@ -7,12 +8,19 @@ const app = express ();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
-});
+// Connect to database
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'adam',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
